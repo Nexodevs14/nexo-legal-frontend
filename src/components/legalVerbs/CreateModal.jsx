eb2721c0f36fc6647ec3f060e83c8bc8ab3cdbd4
ALCAMOjr <alfredocastellanoula.14@gmail.com>
2025-07-01 18:03:44 -0600

    Add the value (Física) to the evidence field set in the requirements table.

--- src/components/legalVerbs/CreateModal.jsx
+++ src/components/legalVerbs/CreateModal.jsx
@@ -194,7 +194,7 @@ function CreateModal({ config }) {
                       input: "resize-y min-h-[150px] py-1 px-2 w-full text-xs text-gray-900 bg-transparent border-b-2 border-gray-300 focus:outline-none focus:ring-0 focus:border-primary peer",
                     }}
                     label="Traducción"
-                    placeholder="Escribir la Traduccíon."
+                    placeholder="Escribir la traducción."
                     variant="bordered"
                   />
                   {translationError && (
